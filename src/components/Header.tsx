4d6730dd7c696d25739eda5f43f277d8dcbc292f
kgruca <kgruca21@gmail.com>
2023-07-14 00:06:21 -0700

    header elems

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
 
 const Header = () => (
-  <nav className="fixed ">
-    <div>
-      <Link href="/">Gruca & Maso</Link>
+  <nav className="sticky top-0 flex h-12 flex-row flex-nowrap items-center justify-between p-6">
+    <div
+      className="mr-6 flex w-full flex-shrink-0 items-center justify-between 
+        text-amber-50"
+    >
+      <Link href="/">
+        <h1>Gruca & Maso</h1>
+      </Link>
+      <div className="flex flex-row flex-nowrap gap-4">
+        <Link href="/login">Login</Link>
+        <Link href="/cart">Shopping Cart</Link>
+      </div>
     </div>
   </nav>
 );
