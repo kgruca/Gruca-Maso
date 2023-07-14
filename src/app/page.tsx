e2b053999478a751bc66642d7d13837e35a6eafd
kgruca <kgruca21@gmail.com>
2023-07-13 21:07:04 -0700

    start on header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Header from "@/components/Header";
 
-export default function Home() {
-  return <h1>Gruca & Maso</h1>;
-}
+const Home = () => (
+  <>
+    <Header />
+  </>
+);
+
+export default Home;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Header = () => (
+  <nav className="fixed ">
+    <div>
+      <Link href="/">Gruca & Maso</Link>
+    </div>
+  </nav>
+);
+
+export default Header;
