b368ef051956aad88d14b7d441f6715fdcb42f2b
kgruca <kgruca21@gmail.com>
2023-07-14 20:22:10 -0700

    top half of header complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import Header from "@/components/Header";
+import { Nunito_Sans } from "@next/font/google";
+import clsx from "clsx";
+
+export const designer = Nunito_Sans({
+  subsets: ["latin"],
+  weight: ["200"],
+});
 
 const Home = () => (
-  <>
+  <div className={designer.className}>
     <Header />
-  </>
+  </div>
 );
 
 export default Home;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,17 @@ import Link from "next/link";
 const Header = () => (
   <nav className="sticky top-0 flex h-12 flex-row flex-nowrap items-center justify-between p-6">
     <div
-      className="mr-6 flex w-full flex-shrink-0 items-center justify-between 
+      className="mr-6 flex w-full shrink-0 items-center justify-between 
         text-amber-50"
     >
-      <Link href="/">
-        <h1>Gruca & Maso</h1>
-      </Link>
+      <div className="flex w-full flex-row justify-center text-6xl mt-20 uppercase">
+        <Link href="/">
+          <h1>Gruca & Maso</h1>
+        </Link>
+      </div>
       <div className="flex flex-row flex-nowrap gap-4">
         <Link href="/login">Login</Link>
-        <Link href="/cart">Shopping Cart</Link>
+        <Link href="/cart">Cart</Link>
       </div>
     </div>
   </nav>
